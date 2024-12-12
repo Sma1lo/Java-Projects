2ca10fcfdf0d1b9e85460cda562da704de48d3bc
Sma1lo_ <135868190+Sma1lo@users.noreply.github.com>
2024-12-12 13:46:08 +0300

    Update BookLibrary.java

--- BookLibrary/BookLibrary.java
+++ BookLibrary/BookLibrary.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class BookLib {
+public class BookLibrary {
     private static List<Book> library = new ArrayList<>();
     
     public static void main(String[] args) {
