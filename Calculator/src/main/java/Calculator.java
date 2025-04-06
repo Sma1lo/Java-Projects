7d92edde449f6fc0131779fcd173ce4a2d528a85
Sma1lo_ <135868190+Sma1lo@users.noreply.github.com>
2025-04-06 17:55:43 +0300

    Add files via upload

---
+++ Calculator/src/main/java/Calculator.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static String calculate(int firstNumber, int secondNumber, String operation) {
+        switch (operation) {
+            case "+":
+                return String.valueOf(firstNumber + secondNumber);
+            case "-":
+                return String.valueOf(firstNumber - secondNumber);
+            case "*":
+                return String.valueOf(firstNumber * secondNumber);
+            case "/":
+                if (secondNumber == 0) {
+                    return "Error: Division by zero";
+                } else {
+                    return String.valueOf(firstNumber / secondNumber);
+                }
+            default:
+                return "Invalid operation";
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            try {
+                System.out.print("Enter the operation (+, -, *, /) or q to quit: ");
+                String operation = scanner.nextLine();
+
+                if (operation.equals("q")) {
+                    System.out.println("Exiting...");
+                    scanner.close();
+                    break;
+                }
+
+                System.out.print("Enter the first number: ");
+                int firstNumber = scanner.nextInt();
+
+                System.out.print("Enter the second number: ");
+                int secondNumber = scanner.nextInt();
+                scanner.nextLine();
+
+                String result = calculate(firstNumber, secondNumber, operation);
+                System.out.println("Answer: " + result);
+
+            } catch (Exception e) {
+                System.out.println("Invalid input: " + e.getMessage());
+                scanner.nextLine();
+            }
+        }
+    }
+}
