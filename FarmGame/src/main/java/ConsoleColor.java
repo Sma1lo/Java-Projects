f11ab69687a3e1833cfd76c070687279597ba146
Sma1lo_ <135868190+Sma1lo@users.noreply.github.com>
2025-09-03 20:27:51 +0300

    Update ConsoleColor.java

--- FarmGame/src/main/java/ConsoleColor.java
+++ FarmGame/src/main/java/ConsoleColor.java
@@ -1,3 +1,8 @@
+/**
+ *
+ *@author Sma1lo
+ */
+
 public class ConsoleColor {
     public static final String RESET = "\u001B[0m";
     public static final String GREEN = "\u001B[32m";
@@ -6,4 +11,5 @@ public class ConsoleColor {
     public static final String CYAN = "\u001B[36m";
     public static final String PURPLE = "\u001B[35m";
     public static final String BLUE = "\u001B[34m";
+
 }
