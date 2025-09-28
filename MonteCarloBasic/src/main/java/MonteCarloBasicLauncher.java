127eb5ef610e4e243fbaebbae169d76f07938060
Sma1lo_ <135868190+Sma1lo@users.noreply.github.com>
2025-09-28 15:03:24 +0300

    Update MonteCarloBasicLauncher.java

--- MonteCarloBasic/src/main/java/MonteCarloBasicLauncher.java
+++ MonteCarloBasic/src/main/java/MonteCarloBasicLauncher.java
@@ -1,5 +1,10 @@
 import javax.swing.*;
 
+/**
+ *
+ *@author Sma1lo
+ */
+
 public class MonteCarloBasicLauncher {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
